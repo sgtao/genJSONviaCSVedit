aee252696da2404a4db76ed44b33c6e6e115bc47
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-16 15:13:37 +0900

    update 02_trial for refactor json2csv.jq file

--- 02_trial_convert/01_JSONtoCSV/01_jsonItem_to_csv.sh
+++ 02_trial_convert/01_JSONtoCSV/01_jsonItem_to_csv.sh
@@ -10,4 +10,4 @@ convert_jsonItem2csv() {
 # main routine
 # convert_jsonItem2csv $1;
   # jq --raw-output 'def walk(k): k as $k | to_entries |  .[] |  {key: ($k + .key), value: .value} |  .key as $key | ( select(.value | type | .=="string" or .=="number") // (.value | walk($key + ".")) );  walk("") | [.key, .value] | @csv ' $1
-jq --raw-output -f json2csv.jq $1
+jq --raw-output -rf json2csv.jq $1
