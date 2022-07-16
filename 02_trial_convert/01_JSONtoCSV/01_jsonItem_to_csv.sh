c57cb7decc7bec6e91a52db17ca33b0de2f0aa19
Shogo Ogami <sg.tao.so@gmail.com>
2022-07-16 10:53:42 +0900

    add 02_trial_convert for convert json to csv

---
+++ 02_trial_convert/01_JSONtoCSV/01_jsonItem_to_csv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# 01_jsonItem_to_csv.sh
+# 
+# 
+convert_jsonItem2csv() {
+  jsonfile=$1;
+  jq -c -r "  def walk(k): k as $k | to_entries |  .[] |  {key: ($k + .key), value: .value} |  .key as $key | ( select(.value | type | .=="string" or .=="number") // (.value | walk($key + ".")) );  walk("") | [.key, .value] | @csv " $jsonfile
+}
+#
+# main routine
+# convert_jsonItem2csv $1;
+  # jq --raw-output 'def walk(k): k as $k | to_entries |  .[] |  {key: ($k + .key), value: .value} |  .key as $key | ( select(.value | type | .=="string" or .=="number") // (.value | walk($key + ".")) );  walk("") | [.key, .value] | @csv ' $1
+jq --raw-output -f json2csv.jq $1
